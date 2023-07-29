2ec4b79c32bdc6aca8277f1bf9236ebe6324b1a4
Yulia Tryhuba <tryhubayulia@gmail.com>
2023-07-30 01:57:56 +0300

    1:54:11

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,30 @@
-import React, { useMemo, useState } from "react";
-// import Counter from "./components/Counter";
-// import ClassCounter from "./components/ClassCounter";
+import React, { useEffect, useState } from "react";
 import './styles/App.css'
-// import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-// import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
-// import MySelect from "./components/UI/select/MySelect";
-// import MyInput from "./components/UI/input/MyInput";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: "AA", body: "WW"},
-    {id: 2, title: "QQ", body: "DD"},
-    {id: 3, title: "CC", body: "NN"}
-  ])
-
+  const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
-
-  const sortedPosts =  useMemo(()=>{
-
-    if(filter.sort){
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  },[filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(()=>{
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+  
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async()=>{
+    const posts = await PostService.getAll()
+    setPosts(posts)
+  })
+  
+  useEffect(()=>{
+    fetchPosts()
+  },[])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -44,8 +35,7 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-// 1:33:43 
-
+// 1:54:11
 
   return (
     <div className="App">
@@ -60,7 +50,13 @@ function App() {
         filter={filter} 
         setFilter={setFilter}
       />
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"POST LIST 1"}/>
+      {postError &&
+        <h1>Error ${postError}</h1>
+      }
+      {isPostsLoading
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+        : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"POST LIST"}/>
+      }
    </div>
   );
 }
